e9c5908efd693534c5ebbe176767597594398e9f
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-23 12:11:10 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <div className="h-[60px] m-4 flex justify-center w-full items-center gap-2 bg-white rounded-lg">
+      <img src="firebase.svg" className="h-10" />
+      <h1 className="font-bold">Firebase Contant App</h1>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/config/firebase-config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB2bh-FPeCL3B7kg53_oTvztDwu_3JHlCA",
+  authDomain: "contact-app-f4663.firebaseapp.com",
+  projectId: "contact-app-f4663",
+  storageBucket: "contact-app-f4663.firebasestorage.app",
+  messagingSenderId: "1058997001527",
+  appId: "1:1058997001527:web:0db39c5e677bb0e97e8eb2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
